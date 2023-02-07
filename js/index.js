e72aebcba70ea0cf67bdbb9fd90990cc89342024
ianthomCO <ian.b.thom@gmail.com>
2023-02-07 00:34:14 -0700

    Add functionality to add symbol to square

--- js/index.js
+++ js/index.js
@@ -1,29 +1,66 @@
 const gameBoard = (()=>{
-    // [[X, X, X],
-    //  [X, X, X],
-    //  [X, X, X]];
-    let board = ['X','X','X','O','O','O','X','X','X'];
+    // [X, X, X,
+    //  X, X, X,
+    //  X, X, X];
+    let _board = Array(9);
 
-    return {board};
+    const reset = () => {
+        for (let i = 0; i < _board.length; i++) {
+            _board[i] = "";
+        }
+    }
+
+    const getIndex = (index) => {
+        return _board[index];
+    }
+
+    const setIndex = (index,sign) => {
+        _board[index] = sign;
+    }
+
+    return {reset, getIndex, setIndex};
 })();
 
-const displayControl = (()=>{
-
-})();
-
-const player = (name,shape) => {
-
-    return {name,shape};
+const player = (sign) => {
+    return {sign};
 };
 
-function displayGameboard(){
+const displayControl = (()=>{
+    // 
+    let _round = 1;
+    let _player1 = player("X");
+    let _player2 = player("O");
+
     const gameCards = document.querySelectorAll(".game-card");
 
     gameCards.forEach((card)=>{
-        const index = card.dataset.index;
-        console.log(gameBoard.board[index])
+        card.addEventListener("click",()=>{
 
-        card.textContent = gameBoard.board[index];
-    })
+            const index = card.dataset.index;
+            let symbol = _round % 2 === 0 ? _player2.sign : _player1.sign;
+            gameBoard.setIndex(index,symbol);
+            displayBoard();
+            _round++;
 
-};
+        });
+    });
+
+    const displayBoard = function() {
+        gameCards.forEach((card)=>{
+            const index = card.dataset.index;
+            card.textContent = gameBoard.getIndex(index);
+        });
+    };
+
+
+
+    // const resetBtn = document.querySelector(".reset-btn");
+    // resetBtn.addEventListener("click",()=>{
+    //     gameBoard.reset();
+    //     displayBoard();
+    // });
+
+
+    return{displayBoard};
+})();
+
