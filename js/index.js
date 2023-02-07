b6c8cb10657e13e512e2bd96b51c2bee8416bfdf
ianthomCO <ian.b.thom@gmail.com>
2023-02-06 19:33:37 -0700

    Create gameboard, player, and control modules

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const gameBoard = (()=>{
+    // [[X, X, X],
+    //  [X, X, X],
+    //  [X, X, X]];
+    let board = [[X,O,X],[O,X,O],[X,O,X]];
+
+    return {board};
+})();
+
+const displayControl = (()=>{
+
+})();
+
+const player = (name,shape) => {
+
+    return {name,shape};
+};
+
