e14ac334031d149076ce36298c511aed6908a68e
ianthomCO <ian.b.thom@gmail.com>
2023-02-09 18:36:14 -0700

    Add comments for functions

--- js/index.js
+++ js/index.js
@@ -4,16 +4,19 @@ const gameBoard = (()=>{
     //  X, X, X];
     let _board = Array(9);
 
+    // Reset board to blank
     const reset = () => {
         for (let i = 0; i < _board.length; i++) {
             _board[i] = "";
         }
     }
 
+    // Get the value of the index of the board
     const getIndex = (index) => {
         return _board[index];
     }
 
+    // Set the value of the index of the board with the specified sign
     const setIndex = (index,sign) => {
         _board[index] = sign;
     }
@@ -21,20 +24,23 @@ const gameBoard = (()=>{
     return {reset, getIndex, setIndex};
 })();
 
+// Function to create a player with a specific sign
 const player = (sign) => {
     return {sign};
 };
 
 const gameController = (()=>{
-    // 
+    // Initialize the round and player symbols
     let _round = 1;
     let _player1 = player("X");
     let _player2 = player("O");
 
+    // Get the sign of the current player
     const getCurrentPlayerSign = function(){
         return _round % 2 === 0 ? _player2.sign : _player1.sign;
     };
 
+    // Script to play a single round of tic-tac-toe
     const playRound = function(index){
         gameBoard.setIndex(index,getCurrentPlayerSign());
         displayControl.displayBoard();
@@ -42,6 +48,7 @@ const gameController = (()=>{
         _round++;
     };
 
+    // Check if the game is over
     const checkGameStatus = function(index){
         const _winConditions =[
             [0,1,2],
@@ -71,6 +78,7 @@ const displayControl = (()=>{
 
     const gameCards = document.querySelectorAll(".game-card");
 
+    // Add listener to each game cell for when it is clicked
     gameCards.forEach((card)=>{
         card.addEventListener("click",()=>{
             const index = card.dataset.index;
@@ -80,6 +88,7 @@ const displayControl = (()=>{
         });
     });
 
+    // Function to display the game board
     const displayBoard = function() {
         gameCards.forEach((card)=>{
             const index = card.dataset.index;
